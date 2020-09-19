4634e22cdc7b87a069b1d289feedecdc4c72600c
ImranYouna <imranyounaskaa123@gmail.com>
2020-09-19 08:31:51 +0500

    one more statement i adde

--- hello.java
+++ hello.java
@@ -2,5 +2,8 @@ public class hello{
 public static void main(String args[]){
 System.out.println("Hello word");
 System.out.println("Imran");
+if(imran==kanza){
+System.out.println("dafa ho jaa kanza");
+
 }
 }
