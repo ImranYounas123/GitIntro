1388e34a277ce312b9477fbada0e0788f7e51fa1
ImranYouna <imranyounaskaa123@gmail.com>
2020-09-19 08:36:44 +0500

    i again changed some statement

--- hello.java
+++ hello.java
@@ -4,6 +4,13 @@ System.out.println("Hello word");
 System.out.println("Imran");
 if(imran==kanza){
 System.out.println("dafa ho jaa kanza");
+System.out.println("Hello imran");
+System.out.println("Hello hasnain");
+System.out.println("Hello moiz");
+System.out.println("Hello hashir");
+System.out.println("Hello mubi");
+System.out.println("Hello kanza");
+System.out.println("Hello wajjo");
 
 }
 }
